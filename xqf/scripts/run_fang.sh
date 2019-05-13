0cd8c62e6815d0a6b3563988d16b1568ede4d953
Vector Li <huanli@redhat.com>
2019-05-13 19:09:31 +0800

    Add scripts to calculate shoufu

---
+++ xqf/scripts/run_fang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+base_max=${1:-"630"}
+base_min=${2:-"550"}
+
+n=$(( (base_max - base_min) / 5 ))
+for (( i = 0; i <= n; i++ )); do
+	total=$(( base_max - 5 * i ))
+	echo "=========================$total================================="
+	for j in 0.90 0.91 0.92 0.93 0.94 0.95; do
+		echo "********* $total::$j *********"
+		python3 ./fang $total $j 20
+		echo
+	done
+	echo
+done

854f51df31f06497fc20aa0ca2aea7a1583a8104
Vector Li <huanli@redhat.com>
2019-05-17 18:01:59 +0800

    Update run_fang.sh to support variable interval

--- xqf/scripts/run_fang.sh
+++ xqf/scripts/run_fang.sh
@@ -2,11 +2,12 @@
 
 base_max=${1:-"630"}
 base_min=${2:-"550"}
+interval=${3:-"5"}
 
 echo '```Bash'
-n=$(( (base_max - base_min) / 5 ))
+n=$(( (base_max - base_min) / interval ))
 for (( i = 0; i <= n; i++ )); do
-	total=$(( base_max - 5 * i ))
+	total=$(( base_max - interval * i ))
 	echo "=========================$total================================="
 	for j in 0.90 0.91 0.92 0.93 0.94 0.95; do
 		echo "********* $total::$j *********"

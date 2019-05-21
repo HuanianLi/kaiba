79315133aca67cbc99e53f3bffa96dc5b5c1b188
Vector Li <huanli@redhat.com>
2019-05-21 17:00:07 +0800

    Update fang.py to support case Shuang575

---
+++ xqf/scripts/Shuang575.md
@@ -0,0 +1,30 @@
+```Bash
+bash[1]$ ./fang 575 0.95 20 N
+Raw total    =   575.00
+Net total    =   546.25
+Loan         =   218.00
+Deed tax     =     5.46
+Geshui       =     5.46
+Zhongjiefee  =    14.75
+Shoufu       =   383.00
+XXCore       =    49.00
+Yuegong(#20) = 15467.65
+Yuegong(#25) = 13886.30
+
+bash[2]$ ./bank 18:0.375:2:ZH
+[ ZhaoHang ] Total Loan = 18w
+[ ZhaoHang ] Total Lixi = 16200
+[ ZhaoHang ] YueLilv    = 0.375
+[ ZhaoHang ] nYears     = 2
+[ ZhaoHang ] YueBenjin  = 7500
+[ ZhaoHang ] YueLixi    = 675
+[ ZhaoHang ] Yuegong    = 8175
+
+###############################################################################
+#            Total Loan      = 18w
+#            Total Lixi      = 16200
+#            Total YueBenjin = 7500
+#            Total YueLixi   = 675
+#            Total Yuegong   = 8175
+###############################################################################
+```
--- xqf/scripts/fang.py
+++ xqf/scripts/fang.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-g_capital = 420 - 67 - 10 #+ 17
+g_resv_yajin = 10
+g_resv_xuefei = 16.8 / 2
+g_resv_zhuanxiu = 20
+g_cbc = 16 + 3
+g_capital = 420 - 67 - g_resv_yajin - g_resv_xuefei - g_resv_zhuanxiu + g_cbc
 g_ylilv = 0.049 * 1.2     # 2nd, increase 20%
 
 
@@ -24,27 +28,35 @@ def get_yuegong(loan, loan_nyears, y_lilv):
 
 
 def main(argc, argv):
-    if argc != 4:
-        sys.stderr.write("Usage: %s <total> <eval_percent> <N>\n" % argv[0])
-        sys.stderr.write("e.g.   %s 630 0.90 20\n" % argv[0])
+    if argc != 5:
+        sys.stderr.write("Usage: %s <total> <eval_percent> <Nyears> <Y|N>\n" %
+                         argv[0])
+        sys.stderr.write("e.g.   %s 630 0.90 20 Y\n" % argv[0])
+        sys.stderr.write("       %s 575 0.95 20 N\n" % argv[0])
         return 1
 
     raw_total    = int(argv[1])
     eval_percent = float(argv[2])
     loan_nyears  = int(argv[3])
+    flag_manwuweiyi = argv[4]
     net_total    = raw_total * eval_percent
-    loan         = net_total * 0.4   # raw_total * eval_percent * 40%
-    deed_tax     = net_total * 0.01  # net_total * 1%
-    zhongjiefee  = raw_total * 0.027 # raw_total * 2.7%
+    loan         = int(net_total * 0.4)     # raw_total * eval_percent * 40%
+    deed_tax     = net_total * 0.01         # net_total * 1%
+    if flag_manwuweiyi == "N":
+        geshui   = net_total * 0.01         # also net_total * 1%
+    else:
+        geshui   = 0
+    zhongjiefee  = raw_total * 0.027 * 0.95 # raw_total * 2.7%
     others       = (600 + 1070) / 10000.0
-    shoufu       = raw_total - loan + deed_tax + zhongjiefee + others
+    shoufu       = raw_total - loan + deed_tax + geshui +zhongjiefee + others
     print("Raw total    = %8.2f" % raw_total)
     print("Net total    = %8.2f" % net_total)
     print("Loan         = %8.2f" % loan)
     print("Deed tax     = %8.2f" % deed_tax)
+    print("Geshui       = %8.2f" % geshui)
     print("Zhongjiefee  = %8.2f" % zhongjiefee)
-    print("Shoufu       = %8.2f" % shoufu)
-    print("XXCore       = %8.2f" % (shoufu - g_capital))
+    print("Shoufu       = %8.2f" % round(shoufu))
+    print("XXCore       = %8.2f" % round(shoufu - g_capital))
     yg01 = get_yuegong(10000 * loan, loan_nyears, g_ylilv);
     print("Yuegong(#%2d) = %8.2f" % (loan_nyears, yg01));
     yg02 = get_yuegong(10000 * loan, loan_nyears + 5, g_ylilv);

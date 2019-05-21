463d31136c6c6f2e8f4d7e613eb42564007ffb09
Vector Li <huanli@redhat.com>
2019-05-21 17:00:07 +0800

    Update fang.py to support case Shuang575

--- xqf/scripts/run_fang.sh
+++ xqf/scripts/run_fang.sh
@@ -11,7 +11,7 @@ for (( i = 0; i <= n; i++ )); do
 	echo "=========================$total================================="
 	for j in 0.90 0.91 0.92 0.93 0.94 0.95; do
 		echo "********* $total::$j *********"
-		python3 ./fang $total $j 20
+		python3 ./fang $total $j 20 Y
 		echo
 	done
 	echo
